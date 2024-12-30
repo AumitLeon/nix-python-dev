82c35e9882e6621f9e17c37451b7032d5f23409a
Aumit Leon <aumitleon@gmail.com>
2024-12-29 22:05:32 -0500

    ENH: streamline python flake

--- flake.nix
+++ flake.nix
@@ -5,50 +5,28 @@
   inputs = {
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  # Flake outputs
-  outputs = { 
-    self,     
-    nixpkgs-unstable,
-    nixpkgs-stable
-  }:
-    let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs-stable.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs-stable { inherit system; };
-      });
-    in
-    {
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default =
-          let
-            # Use Python 3.13
-            python = pkgs.python313;
-          in
-          pkgs.mkShell {
-            # The Nix packages provided in the environment
-            packages = [
-              # Python plus helper tools
-              (python.withPackages (ps: with ps; [
-                virtualenv # Virtualenv
-                pip # The pip installer
-              ]))
-            ];
-            shellHook = ''
+  outputs = { self, nixpkgs-unstable,  nixpkgs-stable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs-stable.legacyPackages.${system};
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            # 👇 we can just use `rustToolchain` here:
+             buildInputs = (with pkgs.python313Packages; [
+                virtualenv
+                pip
+             ]);
+             shellHook = ''
               echo "🐍 You're entering our dev environment! 🐍"
               source python-nix-venv/bin/activate
             '';
           };
-      });
-    };
+        }
+      );
 }
